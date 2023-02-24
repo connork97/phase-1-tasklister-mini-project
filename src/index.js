c2f5b23947b777ef632d7d19a26c6a256a46410b
Connor Kormos <connorkormos97@gmail.com>
2023-02-23 17:49:05 -0800

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTodoList(e.target.newTaskDescription.value)
+    form.reset()
+  })  
 });
+
+const createTodoList = (task) => {
+  const tasks = document.querySelector('#tasks')
+  const p = document.createElement('p')
+  const btn = document.createElement('button')
+  p.textContent = `${task} `
+  btn.textContent = 'Delete'
+  tasks.appendChild(p).appendChild(btn)
+  btn.addEventListener('click', deleteButton)
+}
+
+const deleteButton = (e) => {
+  e.target.parentNode.remove()
+}
